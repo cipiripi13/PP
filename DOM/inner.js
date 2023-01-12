eb7222d71223374c3f7113523800a181c9aab80b
cipiripi13 <krstickatarina1990@gmail>
2023-01-12 13:41:35 +0100

    Changes to inner.js file

--- DOM/inner.js
+++ DOM/inner.js
@@ -1,16 +1,23 @@
+
+// I nacin preko inner html-a
+
 function createDropdown (){
-   
+//    var divovi = document.querySelectorAll('div');
+//    divovi[0].innerHTML = "<select name='drop' id='drop1'><option value='1st option'>1st option</option><option value='2nd option'>2nd option</option><option value='3rd option'>3rd option</option></select>";
    document.querySelector('div').innerHTML= "<select name='drop' id='drop1'><option value='1st option'>1st option</option><option value='2nd option'>2nd option</option><option value='3rd option'>3rd option</option></select>";
   
 
 }
-(createDropdown(['Optione 1', 'Option 2', 'Option 3']));
+(createDropdown());
+
+
+//II nacin
 
 function createDropdownSecond (array){
     var divTwo = document.getElementsByTagName('div')[1];
     var dropdown = document.createElement('select');
    
-   // u div 1 dodajem dropdown
+   // u div 2 sa indeksom jedan dodajem dropdown
     divTwo.appendChild(dropdown);
 
     //dodavanje opcija u dropdown
