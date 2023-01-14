2516e1f014c693cfdc301ea616889ce34d8819c8
cipiripi13 <krstickatarina1990@gmail>
2023-01-14 13:52:10 +0100

    made chages in inheritance.js

--- Objects/Prototype and prototype chain/inheritance.js
+++ Objects/Prototype and prototype chain/inheritance.js
@@ -10,36 +10,8 @@ function Persone (name, surname){
     this.name = name;
     this.surname = surname;
 }
-var osobaMilos = new Persone ('Milos', 'Milosevic');
-console.log(osobaMilos);  //Persone { name: 'Milos', surname: 'Milosevic' }
-
-function Employee(job, salary){
-    this.job = job;
-    this.salary = salary;
-    // this.getData = function(){
-    //     return (`${this.name}, ${this.surname}, ${this.salary}`);
-    // };
-    
-  
-}
-Employee.prototype.getData = function(){
-    return (`${this.name}, ${this.surname}, ${this.salary}`);
-};
-
-
-Employee.prototype.getSalary = function(){
-    console.log(`The salary is ${this.salary}`);
-} 
-
-
-Employee.prototype.increaseSalary = function(){
-    var increased = this.salary + this.salary*10/100;
-    console.log(`The increased salary by 10% is ${increased} `);
-}
-var milosEmploye = new Employee ('accountant', 400);
-console.log(milosEmploye);
-console.log(milosEmploye.getData()); 
-
+// var osobaMilos = new Persone ('Milos', 'Milosevic');
+// console.log(osobaMilos);  //Persone { name: 'Milos', surname: 'Milosevic' }
 
 // nasledjivanje Employee od Persone
 function Employee (name, surname, job, salary){
@@ -49,28 +21,34 @@ function Employee (name, surname, job, salary){
 }
 
 Employee.prototype = Object.create(Persone.prototype);
-Employee.prototype.constructor = Persone;
+Employee.prototype.constructor = Employee;
 
 Employee.prototype.getData = function(){
     return (`${this.name}, ${this.surname}, ${this.salary}`);
 }
-
-var marko = new Employee('Marko', 'Markovic', 'developer', 300);
-console.log(marko.getData());
-console.log(marko);
-
-var milosEmploye = new Employee ('Milos', 'Milosevic', 'accountant', 400);
-console.log(milosEmploye);
-console.log(milosEmploye.getData()); // vrati za ime  i prezime undefined jer jos nije doslo do nasledjivanja
-
-
-
-function Developer (specialization){
-    this.specialization = specialization;
-    this.getSpecialization = function(){
-        console.log(this.specialization);
-    }
+Employee.prototype.getSalary = function(){
+    console.log(this.salary);
 }
+Employee.prototype.increaseSalary = function(){
+    return this.salary + this.salary*10/100;
+}
+
+// var marko = new Employee('Marko', 'Markovic', 'developer', 300);
+// console.log(marko.getData());
+// console.log(marko);
+
+// var milosEmploye = new Employee ('Milos', 'Milosevic', 'accountant', 400);
+// console.log(milosEmploye);
+// console.log(milosEmploye.getData()); // vrati za ime  i prezime undefined jer jos nije doslo do nasledjivanja
+
+
+
+// function Developer (specialization){
+//     this.specialization = specialization;
+//     this.getSpecialization = function(){
+//         console.log(this.specialization);
+//     }
+// }
 
 function Developer(name, surname, job, salary, specialization){
     Employee.call(this, name, surname, job, salary);
@@ -79,25 +57,46 @@ function Developer(name, surname, job, salary, specialization){
 Developer.prototype = Object.create(Employee.prototype);
 Developer.prototype.constructor = Developer;
 
-var jovanDeveloper = new Developer('Jovan', 'Jovanovic', 'developer', 600, 'JS');
-console.log(jovanDeveloper);
-console.log(jovanDeveloper.getSpecialization);
-
-function Manager (department){
-    this.department = department; 
-    
+Developer.prototype.getSpecialization = function(){
+    console.log(this.specialization);
 }
+
+// var jovanDeveloper = new Developer('Jovan', 'Jovanovic', 'developer', 600, 'JS');
+// console.log(jovanDeveloper);
+// console.log(jovanDeveloper.getSpecialization);
+
+// function Manager (department){
+//     this.department = department; 
+    
+// }
+
+function Manager(name, surname, job, salary, department){
+    Employee.call(this, name,surname, job, salary);
+    this.department = department;
+}
+Manager.prototype = Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+
 Manager.prototype.getDepartment = function(){
     console.log(`Department name is ${this.department}`);
 };
 Manager.prototype.changeDepartmet = function(x){
-        var newD = this.x;
-        console.log(`The new department is ${newD}`);
+        this.department =x;
+        console.log(`The new department is ${x}`);
     }
 ;
 
-var it = new Manager('HR');
-it.getDepartment();
+var persone = new Persone('Pero', 'Peric');
+var jovanaEmploye = new Employee('Jovana', 'Jovanovic', 'accountant', 300);
+var kacaDeveloper = new Developer('Kaca', 'K', 'developer', 800, 'js');
+var djoleManager = new Manager('Djole', 'Z', 'manager', 2000, 'construction');
 
-var newDepartmwnt = new Manager('accounting');
-newDepartmwnt.changeDepartmet();   
+console.log(persone);
+console.log(jovanaEmploye);
+console.log(kacaDeveloper);
+console.log(djoleManager);
+
+djoleManager.changeDepartmet('cars');
+djoleManager.getDepartment();
+
+console.log(djoleManager.increaseSalary()); //2200 vrati
