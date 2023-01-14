62d7391c65be11188f84c557aa84827485f13470
cipiripi13 <krstickatarina1990@gmail>
2023-01-14 14:01:16 +0100

    changes in app.js

--- Objects/Prototype and prototype chain/exerciseApp.js
+++ Objects/Prototype and prototype chain/exerciseApp.js
@@ -1,25 +1,33 @@
-function AllApps (stars){
+function AllApps (name, licence, stars){
+    this.name = name;
+    this.licence = licence;
     this.stars = stars;
 }
 AllApps.prototype.getData = function(){
-    console.log(`The name of app is ${this.name}, the adress is ${this.url}, ${this.technologies}, ${this.licence}, ${this.stars}`);
+    console.log(`The name of app is ${this.name},  ${this.licence}, an it has ${this.stars} stars`);
+}
+
+AllApps.prototype.isCCLicence = function(){
+    return this.licence === 'CC';
+}
+
+AllApps.prototype.likes = function(){
+    return this.stars++;
+}
+AllApps.prototype.showStars = function(){
+    console.log(this.stars);
 }
 
 
 
 function WebApp (name, url, technologies, licence, stars){
-    this.name = name;
-    this.url =url;
-    this.technologies = technologies;
-    this.licence = licence;
-    this.stars = stars;
-
-
+   AllApps.call(this,name, licence, stars );
+   this.url = url;
+   this.technologies = technologies;
+   
 }
 
 function MobileApp (name, platform, licence, stars){
-    this.name = name;
+    AllApps.call(this, name, licence, stars);
     this.platform = platform;
-    this.licence= licence;
-    this.stars = stars;
 }
