a67578fca6124c319a41a16a6d6508b8609e7374
cipiripi13 <krstickatarina1990@gmail>
2023-01-16 13:25:39 +0100

    uppdating superMario game

--- Events/Super Mario/mario.js
+++ Events/Super Mario/mario.js
@@ -2,33 +2,42 @@
 var mario = document.getElementById('mario');
 var runningMario = document.getElementById('running-mario');
 var bg = document.querySelector('.container');
-var ground = document.querySelectorAll('div')[1];
+
+
 var x = 0;
-var y = 0;
+var moveBg;
 document.addEventListener('keydown', function (event){
     // dok je key na desno mario slika se ne prikazuje i display none
    if(event.key === 'ArrowRight'){
     mario.style.display = 'none';
    // dok se ovde gif prikazuje
-    runningMario.style.display = 'block';
-    ground.classList.add('ground');
-
-
-    // var position = parseInt(bg.style.backgroundPosition) || 0;
-    // bg.style.backgroundPosition = (position - 15) + 'px';
-
-
+   runningMario.style.display = 'block';
+    
+   //sta se desava sa pozadinom kada je keydown aktivan
+   moveBg = setInterval(function(){
+    x -=1;
+    bg.style.backgroundPosition = x + 'px bottom';
+  }, 1);
+    
      } else{
     mario.style.display = 'block';
+    
    }
 
 });
 
 document.addEventListener("keyup", function(event) {
-    if (event.key === "ArrowRight") {
+    // if (event.key === "ArrowRight") {
+      // prikazi sliku
       mario.style.display = "block";
-      runningMario.style.display = "none";  
+      //ukloni gif
+      runningMario.style.display = "none"; 
+      //skloni interval
+      clearInterval(moveBg); 
+      //event.stopPropagation()
+      
+      
       
     }
-  })
+  )
 
